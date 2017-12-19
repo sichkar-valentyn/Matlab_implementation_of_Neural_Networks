5bed7c4f86d324bbcbb41b5e31f3719c471d6e85
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2017-12-19 16:47:12 +0300

    Update license
    Add extra information about references

--- NN_Matrices.m
+++ NN_Matrices.m
@@ -5,6 +5,8 @@
 % MIT License
 % Copyright (c) 2017 Valentyn N Sichkar
 % github.com/sichkar-valentyn
+% Reference to:
+% [1] Valentyn N Sichkar. Matlab implementation of Neural Networks Results for Control System of five Ultrasonic sensors // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Matlab_implementation_of_Neural_Networks (date of access: XX.XX.XXXX)
 
 close all;
 
